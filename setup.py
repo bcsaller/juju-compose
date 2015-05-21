9973a01d6ddee447bdfb54eb26abc0de3ea3729e
Benjamin Saller <bcsaller@gmail.com>
2015-05-21 14:11:56 -0700

    updates for dynamic tactic loading, tox usage, setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='composer',
+            version='1.0',
+            description='Charm Composition Tooling',
+            author='Juju Solutions Team',
+            author_email='benjamin.saller@canonical.com',
+            url='https://github.com/bcsaller/juju_compose',
+            packages=['juju_compose'],
+           )
